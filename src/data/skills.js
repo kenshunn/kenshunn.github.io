ab1489ef2c4016750d929546a27d39601586d809
kenshunn <aaron.11demartin@gmail.com>
2026-06-30 09:47:04 +0800

    feat: add static skills and projects data

---
+++ src/data/skills.js
@@ -0,0 +1,23 @@
+const skills = [
+  {
+    group: 'Frontend Development',
+    items: [
+      { name: 'HTML', level: 'Newbie' },
+      { name: 'CSS', level: 'Basic' },
+      { name: 'JavaScript', level: 'Newbie' },
+      { name: 'C++', level: 'Basic' },
+      { name: 'C#', level: 'Basic' },
+      { name: 'Python', level: 'Basic' },
+    ],
+  },
+  {
+    group: 'Backend Development',
+    items: [
+      { name: 'Git', level: 'Newbie' },
+      { name: 'Node JS', level: 'Newbie' },
+      { name: 'SQL', level: 'Newbie' },
+    ],
+  },
+];
+
+export default skills;
